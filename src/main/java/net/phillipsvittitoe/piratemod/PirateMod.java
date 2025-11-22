7c0614059053589ba0092e50b86c81ce5f5fa46b
Carmindy-P <cphillips30@murraystate.edu>
2025-11-22 13:29:03 -0600

    Initial commit

---
+++ src/main/java/net/phillipsvittitoe/piratemod/PirateMod.java
@@ -0,0 +1,56 @@
+package net.phillipsvittitoe.piratemod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(PirateMod.MOD_ID)
+public final class PirateMod {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "piratemod";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public PirateMod(FMLJavaModLoadingContext context) {
+        var modBusGroup = context.getModBusGroup();
+
+        // Register the commonSetup method for modloading
+        FMLCommonSetupEvent.getBus(modBusGroup).addListener(this::commonSetup);
+
+        // Register the Deferred Register to the mod event bus so blocks get registered
+        //BLOCKS.register(modBusGroup);
+        // Register the Deferred Register to the mod event bus so items get registered
+        //ITEMS.register(modBusGroup);
+        // Register the Deferred Register to the mod event bus so tabs get registered
+        //CREATIVE_MODE_TABS.register(modBusGroup);
+
+        // Register the item to a creative tab
+        BuildCreativeModeTabContentsEvent.BUS.addListener(PirateMod::addCreative);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    // Add the example block item to the building blocks tab
+    private static void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
